513609179d2fb0a32fc823ee46d347c781d06d7a
Xuyic <96169364+Xuyic@users.noreply.github.com>
2024-05-16 00:28:23 +0800

    Update reply-message.js

--- utils/reply-message.js
+++ utils/reply-message.js
@@ -1,12 +1,13 @@
 import config from '../config/index.js';
 import { reply } from '../services/line.js';
 
-const replyMessage = ({
+const replyMessage = async ({
   replyToken,
   messages,
 }) => {
+  console.log('Inside replyMessage', { replyToken, messages }); // 日誌
   if (config.APP_ENV !== 'production') return { replyToken, messages };
-  return reply({ replyToken, messages });
+  return await reply({ replyToken, messages });
 };
 
 export default replyMessage;
